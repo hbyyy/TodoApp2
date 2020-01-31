59128d7103b6066fd8c951a54904b47ea9e83f21
hbyyy <qjaduddl94@gmail.com>
2020-01-31 22:52:01 +0900

    메인 페이지 수정, 완료, 취소 버튼 추가

--- app/todos/urls.py
+++ app/todos/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
-from todos.views import todolist_view, todo_create
+from todos.views import todolist_view, todo_create, todo_finish_view, todo_cancel_view
 
 app_name = 'todos'
 urlpatterns = [
     path('', todolist_view, name='todo-list'),
-    path('create/', todo_create, name='todo-create')
+    path('create/', todo_create, name='todo-create'),
+    path('finish/<int:todo_id>', todo_finish_view, name='todo-finish'),
+    path('cancel/<int:todo_id>', todo_cancel_view, name='todo-cancel'),
 ]
--- app/todos/views.py
+++ app/todos/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render, redirect
-
 # Create your views here.
 from django.utils import timezone
 
@@ -11,10 +10,13 @@ def todolist_view(request):
     todos = Todo.objects.filter(user=request.user
                                 ).filter(created_date__day=timezone.localtime().day
                                          ).order_by('created_date')
+    todos_finish = todos.filter(is_done=True)
+    todos_not_finish = todos.filter(is_done=False)
     form = TodosForm()
     context = {
         'form': form,
-        'todos': todos,
+        'todos_finish': todos_finish,
+        'todos_not_finish': todos_not_finish,
         'time': timezone.localtime()
     }
     return render(request, 'todos/main.html', context)
@@ -26,3 +28,13 @@ def todo_create(request):
         if form.is_valid():
             form.save(request.user.id)
         return redirect('todos:todo-list')
+
+
+def todo_finish_view(request, todo_id):
+    Todo.objects.filter(id=todo_id).update(is_done=True)
+    return redirect('todos:todo-list')
+
+
+def todo_cancel_view(request, todo_id):
+    Todo.objects.filter(id=todo_id).update(is_done=False)
+    return redirect('todos:todo-list')
